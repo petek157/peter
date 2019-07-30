f27151f498291a6bca55ffd5fa8eab6b42fb6e47
petek157 <petek157@hotmail.com>
2019-07-30 16:40:14 -0500

    append cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,13 @@ set :repo_url, "https://github.com/petek157/peter.git"
 set :git_http_username, Rails.application.credentials.dig(:github, :user)
 set :git_http_password, Rails.application.credentials.dig(:github, :pass)
 
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-
+append :linked_files, 'config/master.key'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
